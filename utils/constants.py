77ea99ba9b7161d73b93e092a71d18eccaac4b79
javialvarezdrive <153924249+javialvarezdrive@users.noreply.github.com>
2025-02-27 13:50:38 +0100

    Update constants.py

--- utils/constants.py
+++ utils/constants.py
@@ -1,1 +1,28 @@
+# utils/constants.py
 
+ACTIVITIES_LIST = [
+    "Defensa Personal",
+    "Acondicionamiento Físico",
+    # ... (añade más actividades aquí)
+]
+
+TIME_SLOTS = [
+    "Mañana",
+    "Tarde",
+    "Noche"
+]
+
+SECTIONS_LIST = [
+    "Motorista",
+    "Patrullas",
+    "GOA",
+    "Atestados",
+    # ... (añade más secciones si es necesario)
+]
+
+GROUPS_LIST = [
+    "G-1",
+    "G-2",
+    "G-3",
+    # ... (añade más grupos si es necesario)
+]
